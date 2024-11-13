b657bd416db7f323e0742ac9ab26eb7bc11ad197
goodtimes0504 <revolution_lc@outlook.com>
2024-11-13 12:29:20 +0800

    调整了下拉菜单 实现退出登录

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -18,17 +18,19 @@
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
             <el-dropdown-item>
-              Home
+              首页
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
+          <a target="_blank" href="https://github.com/goodtimes0504/heimahr">
+            <el-dropdown-item>项目地址</el-dropdown-item>
           </a>
           <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
+            <el-dropdown-item>修改密码</el-dropdown-item>
           </a>
-          <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+          <!-- native事件修饰符 注册组件的根元素的原生事件 -->
+          <!-- el-dropdown-item是没有点击事件的 -->
+          <el-dropdown-item @click.native="logout">
+            <span style="display:block;">退出登录</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -59,8 +61,11 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
+      // 调用退出登录action
       await this.$store.dispatch('user/logout')
+      // 跳转到登录页 并携带当前页面的路径
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      // alert('退出登录')
     }
   }
 }
