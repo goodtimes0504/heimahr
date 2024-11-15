f638e781d4895429c0219ea3071652385dd23b7d
goodtimes0504 <revolution_lc@outlook.com>
2024-11-15 15:25:22 +0800

    实现员工表格和分页结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -13,7 +13,28 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table>
+          <el-table-column align="center" label="头像" />
+          <el-table-column label="姓名" />
+          <el-table-column label="手机号" sortable />
+          <el-table-column label="工号" sortable />
+          <el-table-column label="聘用形式" />
+          <el-table-column label="部门" />
+          <el-table-column label="入职时间" sortable="" />
+          <el-table-column label="操作" width="280px">
+            <template>
+              <el-button type="text" size="mini">查看</el-button>
+              <el-button type="text" size="mini">角色</el-button>
+              <el-button type="text" size="mini">删除</el-button>
+            </template>
+          </el-table-column>
+
+        </el-table>
         <!-- 分页 -->
+        <!-- align意思是垂直居中 flex是水平排列 justify 是水平右侧对齐 -->
+        <el-row style="height:60px" align="middle" type="flex" justify="end">
+          <el-pagination layout="total,prev, pager, next" :total="1000" />
+        </el-row>
       </div>
     </div>
   </div>
